2c56924eb3c5f753d89f1f0c6e7467245b4c86f2
chipx0r <chipx0r@gmail.com>
2018-11-11 17:22:45 -0600

    Solo 1 test

--- test/test_catalogos_sat.rb
+++ test/test_catalogos_sat.rb
@@ -2,14 +2,14 @@ require 'minitest/autorun'
 require_relative 'catalogos_sat'
 
 class CatalogosTest < Minitest::Test
-  def test_modulos
-    myTest = Catalogos.new()
-    assert(myTest.descargar)
-    assert(myTest.procesar)
-    assert(myTest.nuevo_xls? == false)    
-    assert(myTest.nuevo_xls?("test"))
-        
-  end
+#  def test_modulos
+#    myTest = Catalogos.new()
+#    assert(myTest.descargar)
+#    assert(myTest.procesar)
+#    assert(myTest.nuevo_xls? == false)    
+#    assert(myTest.nuevo_xls?("test"))
+#        
+#  end
   def test_main
     myTest = Catalogos.new()
     assert(myTest.main(true))
